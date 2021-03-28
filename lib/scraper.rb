3ec3d54d01927cd7e9a1d3f8924c7a8f967f5bf2
Luciana Sugeta <lulusugeta@gmail.com>
2021-03-27 20:41:18 -0400

    commit tree 5fe4d194b752e7ef54719dfcfae52a151160d586

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,18 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+#doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+#puts doc.css(".headline-260IBN").text.strip
+doc = Nokogiri::HTML(html)
+courses = doc.css(".tout__label.heading.headline--level-4")
+
+courses.each do |course|
+    puts course.text.strip
+end
+
+p doc.css(".tout__label.heading.headline--level-4")[0].name
+# => "h2"
+
+p doc.css(".tout__label.heading.heading--level-4")[0].attributes 
+# => {"class"=>#<Nokogiri::XML::Attr:0x3ff04f8b3754 name="class" value="tout__label heading heading--level-4">} 
+
